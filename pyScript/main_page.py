aa13667f6a345d00a6b9ffd6268a854809850792
GradientFox <53513063+GradientFox@users.noreply.github.com>
2021-07-02 12:54:17 +0500

    Update 0.1.2

--- pyScript/main_page.py
+++ pyScript/main_page.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 blueprint = Blueprint('main_page', __name__)
 
 
-@blueprint.route('/')
+@blueprint.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('main_page.html')
+    if request.method == 'GET':
+        return render_template('main_page.html')
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        print(login, password)
+        return render_template('main_page.html', some_inf=f'Welcome {login}')
--- pyScript/register_page.py
+++ pyScript/register_page.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 
 blueprint = Blueprint('register', __name__)
 
 
-@blueprint.route('/register')
+@blueprint.route('/register', methods=['POST', 'GET'])
 def index():
-    return render_template('register.html')
+    if request.method == 'GET':
+        return render_template('register.html')
+    if request.method == 'POST':
+        if 'register' in request.form:
+            login = request.form['login']
+            password = request.form['password']
+            print(login, password)
+            return render_template('register.html', some_inf=f'You are succesful register, {login}!')
+        else:
+            login = request.form['login']
+            password = request.form['password']
+            print(login, password)
+            return redirect('/')
